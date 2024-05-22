6f1d90d76f7986dfa80c2bb98451829c7ebc26b0
Tamara Pritchard <tamarapritchard@Tamaras-MBP.home>
2024-05-22 15:40:33 -0700

    Lesson 2 assignment

---
+++ react-demo/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Greet, Greet2 } from "./components/Greet";
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Greet2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-demo/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Greet() {
+  return <h1>Hello Tamara</h1>;
+}
+
+const Greet2 = () => {
+  return <h1>Hello Again Tamaa</h1>;
+};
+
+export { Greet, Greet2 };
